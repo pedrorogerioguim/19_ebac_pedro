74e18f074c2b9f4f98d3302907ec17860ee46e13
pedrorogerioguim <pedrorogeriosousa@gmail.com>
2024-02-02 02:32:03 +0000

    arquivo gasolina.py atualizado

---
+++ gasolina.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import seaborn as sns
+
+gasolina_df = pd.read_csv('gasolina.csv', sep=',')
+
+with sns.axes_style ('whitegrid'):
+  grafico_gasolina = sns.lineplot(data=gasolina_df, x='dia', y='venda')
+  grafico_gasolina.set(title='Gasolina em São Paulo/SP nos 10 primeiros dias de Julho/2021', xlabel='Data', ylabel='Venda')
+  grafico_gasolina.get_figure().savefig(f"gasolina.png")
+     
